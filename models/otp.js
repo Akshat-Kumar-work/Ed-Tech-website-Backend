e76312cd99506751251a1236601802c23902687b
Akshat <iamakku0.0.0.1@gmail.com>
2023-06-30 13:31:52 +0530

    otp model and nodemailer function done

--- models/otp.js
+++ models/otp.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const mailsender = require("../utils/mailSender")
 const otpSchema = new mongoose.Schema({
     email:{
         type:String,
@@ -14,6 +14,23 @@ const otpSchema = new mongoose.Schema({
         default:Date.now(),
         expires:5*60
     }
+})
+
+//function to send mail
+async function sendVerificationEmail(email , otp){
+    try{
+        const mailResponse = await mailsender(email , "Verification email", otp);
+        console.log("email sent successfully",mailResponse)
+    }
+    catch(err){
+        console.log("error occured while sending mail",err)
+    }
+}
+
+//pre middleware
+otpSchema.pre("save",async function (next){
+    await sendVerificationEmail(this.email , this.otp);
+    next();
 })
 
 module.exports = mongoose.model("OTP",otpSchema)
