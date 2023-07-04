312559870b9e83c59688fcb8fe10f5cb9389d619
Akshat <iamakku0.0.0.1@gmail.com>
2023-07-04 22:25:07 +0530

    subsection

--- controllers/subSection.js
+++ controllers/subSection.js
@@ -4,11 +4,11 @@ const Section = require("../models/section");
 //create subsection handler
 exports.createSubSection = async(req ,res)=>{
     try{
-
+        //fetching data from req
         const{ sectionId ,  title , timeDurationduration , description   } = req.body;
-
+        // fetching video file 
         const video = req.files.file
-
+        //validation
         if(!sectionId || !title || !timeDuration || !description ||!video){
             
         }
