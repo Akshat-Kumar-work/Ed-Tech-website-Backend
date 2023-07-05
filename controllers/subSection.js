0307f786fb55f15905739f3676eb5e619a8a81ff
Akshat <iamakku0.0.0.1@gmail.com>
2023-07-05 12:19:18 +0530

    subsection handlers done

--- controllers/subSection.js
+++ controllers/subSection.js
@@ -1,6 +1,7 @@
 const SubSection = require("../models/subSection");
 const Section = require("../models/section");
-
+const {ImageUploaderToCloudinary} =require("../utils/imageUploader")
+require("dotenv").config();
 //create subsection handler
 exports.createSubSection = async(req ,res)=>{
     try{
@@ -10,11 +11,105 @@ exports.createSubSection = async(req ,res)=>{
         const video = req.files.file
         //validation
         if(!sectionId || !title || !timeDuration || !description ||!video){
-            
+            return res.status(400).json({
+                success:false,
+                message:"all fields are not filled"
+            })
         }
+        //upload video to cloudinary
+        const uploadDetails = await ImageUploaderToCloudinary(video , process.env.FOLDER_NAME);
+        //create a subsection entry in db
+        const SubSectionDetails = await SubSection.create({
+            title:title,
+            timeDuration:timeDurationduration,
+            description:description,
+            videoUrl:uploadDetails.secure_url
+        })
+        //update the section also
+        const updatedSection = await Section.findByIdAndUpdate( {_id:sectionId} ,
+                                                         {$push:{subSection:SubSectionDetails._id}} ,
+                                                         {new:true}).populate("subSection");
 
+        //return response
+        return res.status(200).json({
+            success:true,
+            message:"sub section created successfully",
+            updatedSection
+        })
    }
         catch(err){
-
+            console.log(err)
+            return res.status(500).json({
+                success:false,
+                message:"internal err , while updating subsection"
+            })
         }
+}
+
+//update subsection handler
+
+const updateSubSection = async (req ,res)=>{
+
+    try{
+        //fetch data
+    const {sectionId , subSectionId,  title , timeDuration , description } = req.body
+    //validations
+    const video = req.files.file
+    //upload video
+    const newupload = await ImageUploaderToCloudinary(video , process.env.FOLDER_NAME);
+    //update to subsection
+    const updatedSubSection = await SubSection.findByIdAndUpdate( subSectionId ,
+                                                        {title:title,
+                                                        timeDuration:timeDuration,
+                                                        description:description,
+                                                        videoUrl:newupload
+                                                        })
+    //update to section also
+    const newSection = await Section.findByIdAndUpdate( {_id:sectionId},
+                                                        {$push:{subSection:updatedSubSection._id}},
+                                                        {new:true}).populate("subSection")
+
+    //return response
+    return res.status(200).json({
+        success:true,
+        message:"sub section updated successfully",
+       
+    })
+    }
+catch(err){
+    console.log(err)
+    return res.status(500).json({
+        success:false,
+        message:"internal err , while updating subsection"
+    })
+}
+}
+
+exports.deletesubSection = async(req,res) =>{
+    try{
+
+        //we are assuming that we are sending id in parameters
+        const{ subsectionId} = req.params;
+
+       if (!subsectionId){
+        return res.status(400).json({
+            success:"false",
+            message:"missing properties"
+        })
+       }
+
+       const newsubSection = await Section.findByIdAndDelete( subsectionId ,{new:true});
+
+       return res.status(200).json({
+        success:true,
+        message:"deleted successfully"
+       })
+    }
+    catch(err){
+        return res.status(500).json({
+            success:false,
+            message:"unable to delete subsection",
+            error:err
+        })
+    }
 }
