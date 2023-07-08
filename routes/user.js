d76054c6bf37355a333b614d33f5a38ca5bbdc71
Akshat <iamakku0.0.0.1@gmail.com>
2023-07-08 23:28:11 +0530

    cloudiary connected

--- routes/user.js
+++ routes/user.js
@@ -10,10 +10,13 @@ const { auth  } = require("../middleware/auth");
 const { login ,signUp , sendOTP , changePassword } = require("../controllers/Auth");
 const { resetPassword , resetPasswordToken } = require("../controllers/resetPassword");
 
-//routes
 
-//authentication routes
 
+
+
+//---------------------------------------------------------routes---------------------------------------------
+
+//--------------------------------------------------authentication routes------------------------------
 // Route for user login
 router.post("/login", login)
 
@@ -25,13 +28,16 @@ router.post("/sendotp", sendOTP)
 
 // Route for Changing the password
 router.post("/changepassword", auth, changePassword)
+//-----------------------------------------------------------------------------------------------------------------
 
 
-//password routes
 
+
+//-----------------------------------------------------password routes------------------------------------
 // Route for generating a reset password token
 router.post("/reset-password-token", resetPasswordToken)
 
 // Route for resetting user's password after verification
 router.post("/reset-password", resetPassword)
+//-----------------------------------------------------------------------------------------------------------------
 
